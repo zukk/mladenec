a53a3936ebfea28c23fa1e4808da4403b0b309f8
zukk <m.zukk@ya.ru>
2016-03-23 13:01:55 +0300

    брать самую маленькую пачку если нет точного совпадения

--- application/db_changes/task_13431_good_dups.sql
+++ application/db_changes/task_13431_good_dups.sql
@@ -1,7 +1,7 @@
 create table good_dups (good_id int unsigned not null, section_id int unsigned not null);
 
 #запросы для теста - генерят категорию Япония в eatmart
-#insert into z_section (vitrina, name, sort, code) values ('ogurchik', 'Япония', 8, 888);
+#insert into z_section (vitrina, name, translit, sort, code) values ('ogurchik', 'Япония', 'japan', 8, 888);
 
 #во всех этих категориях есть японские товары
 #insert into z_section (id, vitrina, name, translit, sort, code, parent_id)
@@ -12,3 +12,16 @@ create table good_dups (good_id int unsigned not null, section_id int unsigned n
 
 #дубли
 #insert into good_dups (good_id, section_id) select id, section_id + 888 from z_good where active = 1 and price > 0 and country_id = 60;
+
+
+#insert ignore into z_section (id, vitrina, name, translit, sort, code, parent_id)
+#  select s.parent_id + 888, 'ogurchik', p.name, concat(p.translit, '-1'), p.sort, s.parent_id + 888, 245831 from z_section s
+
+#  join z_section p ON (p.id = s.parent_id)
+#  where s.id IN (29798, 29293, 91821, 53580, 29494, 28783, 28719, 29585, 28856, 28628, 28836, 29461,
+#   175812, 29891, 43574, 64447, 28962, 29138, 29946, 29982, 28903, 91819, 29962
+#);
+
+#insert into good_dups (good_id, section_id) select g.id, s.parent_id + 888 from z_good g
+#join z_section s on (s.id = g.section_id)
+#where g.active = 1 and g.price > 0 and g.country_id = 60;
