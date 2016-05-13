706ade2e013b646e57b8e962f72df24599e8e838
Artem <artem2102@list.ru>
2016-05-13 18:05:49 +0600

    #task_13786_retailrocket - Не подгружается скрипт RetailRocket(отмена)

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -756,10 +756,6 @@ if ( ! empty($_SERVER['REQUEST_URI'])) {
 		$hh[] = "*.criteo.net"; // criteo
 		$hh[] = "*.criteo.com"; // criteo
 		$hh[] = "*.lenmit.com"; // lenmit
-		$hh[] = "*.moxielinks.com"; // moxielinks
-		$hh[] = "*.luxup.ru"; // luxup
-		$hh[] = "*.mixmarket.biz"; // mixmarket
-
 
         $hh[] = "*.wapstart.ru"; // counters & trackers
         $hh[] = "https://*.doubleclick.net";
