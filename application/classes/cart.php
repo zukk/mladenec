839a8c7117fa337265e460becc1955edd254870a
zukk <zu23chill0>
2016-09-19 20:11:30 +0300

    акция нутрилон

--- application/classes/cart.php
+++ application/classes/cart.php
@@ -1295,6 +1295,9 @@ class Cart {
         reset($return['times']);
         $first_price = current($return['times']);
 
+        $zukk = Model_User::logged() && Model_User::current()->id == 59315;
+        if ($zukk) Log::instance()->add(Log::INFO, 'ZUZU '.__LINE__.$first_price);
+
         // !!! акция нутрилон - бесплатная доставка внутри мкад если есть товары из списка
         if ($first_price > 0 && Model_Zone::locate($latlong, Model_Zone::MKAD)) { // внутри мкад
             $total = DB::select([DB::expr('COUNT(*)'), 'total'])
