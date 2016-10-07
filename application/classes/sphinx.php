a6fe4c7df0062fe712ff09d8506f2901a8f3b39a
zukk <zu23chill0>
2016-10-07 15:01:27 +0300

    filter_not_exists = 0

--- application/classes/sphinx.php
+++ application/classes/sphinx.php
@@ -994,7 +994,7 @@ class Sphinx {
                 Log::instance()->add(Log::INFO, 'redir '.$this->_mode.$this->_query.' to '.$href.'. REASON: '.$redir);
                 $request->redirect($href, 301);
             } // редирект
-        } elseif ($this->_mode == 'tag' && $this->_tag) { {
+        } elseif ($this->_mode == 'tag' && $this->_tag) {
             $this->_tag->filter_not_exists = 0;
             $this->_tag->save();
         }
